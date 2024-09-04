a7cd77c5afc59d5f10de8b8c98cba0bd541f86e1
xabibullo <komilovkhabib2010@gmail.com>
2024-09-04 16:25:54 +0500

    corser

--- web/settings.py
+++ web/settings.py
@@ -77,6 +77,25 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:9000",
 ]
 
+CORS_ALLOW_ALL_ORIGINS:True
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
 
 TEMPLATES = [
     {
